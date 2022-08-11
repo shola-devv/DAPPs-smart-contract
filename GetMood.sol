d6b01ef982af64a4d7b5a8b53cd18162e714d968
Emmanuel <102961506+Emmanuel-web3dev@users.noreply.github.com>
2022-08-11 02:04:14 +0100

    Update and rename README.md to GetMood.sol
    This contract sets a mood and gets it...

---
+++ GetMood.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.1;
+
+
+
+
+contract MyMoodDiary{
+    
+    
+    string mood;
+    
+    
+    function setMood(string memory _mood) public{
+        mood = _mood;
+    }
+    
+    
+    function getMood() public view returns(string memory){
+        return mood;
+    }
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# DAPPs-smart-contract
